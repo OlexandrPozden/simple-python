450cf81bfb05ba48dc7c59efb69b2f2ea25ee855
Alex <pozden.sasha@gmail.com>
2021-03-14 00:29:07 +0200

    Parallax is awful idea

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,27 @@
-let card = document.findElementByclassName("parallax")[0];
+/*let card = document.getElementsByClassName("parallax")[0];
 
-card.addEventListener("mousemove", (e) => {
-    this.querySelectorAll(".parallax-el").forEach(layer => {
-        const speed = layer.getAttribute(".data-speed");
-        const x = (window.innerWidth - e.pageX * speed) / 100;
-        const y = (window.innerHeight - e.pageY * speed) / 100;
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    })
+card.addEventListener('mousemove', (e)=>{
+    console.log("invokes eventlistener");
+    var rect = card.getBoundingClientRect();
+    console.log(rect.top, rect.right, rect.bottom, rect.left);
+    console.log(e.pageX,e.pageY);
+    console.log(this);
+    arr = card.getElementsByClassName("layer");
+    console.log(arr);
+    var array = Array.prototype.slice.call( arr )
+    array.forEach(element => {
+        element.style.color = "green";
+        console.log(element.getAttribute("data-speed"));
+        speed = element.getAttribute("data-speed");
+        console.log(card.clientWidth);
+        innerX = e.pageX - rect.left;
+        innerY = e.pageY - rect.top;
+        const x = (card.clientWidth - innerX * speed) / 100;
+        const y = (card.clientHeight - innerY * speed) / 100;
+        console.log(x,y);
+        
+        element.style.transform = `translateX(${x}px) translateY(${y}px)`;
+
+    });
 });
+*/
