52e9404059394f81e04fa9f3934dcfde35abefa1
Alex <pozden.sasha@gmail.com>
2021-03-27 10:28:45 +0200

    add 404.html to python webserver

--- backend/status_codes.py
+++ backend/status_codes.py
@@ -1,12 +1,22 @@
 import time
+
 def error500(environ, start_response, info=""):
     info_response = "\n"+info
     start_response('500 Internal Server Error',[('Content-type','text/plain')])
     return [b'500 Internal Server Error', info_response.encode()]
 
 def notfound(environ, start_response):
-    start_response('404 Not Found', [('Content-Type','text/plain')])
-    return [b'404 Not Found']
+    code = '404 Not Found'
+    filename = 'static/404.html'
+    try:
+        response_body = ""
+        with open(filename, 'r') as f:
+            response_body = f.read()
+        start_response(code,[('Content-Type','text/html'),('Content-length',str(len(response_body)))])
+        return [response_body.encode()]
+
+    except IOError as err:
+        return error500(environ, start_response, str(err))
 
 
 def ok200(environ, start_response):
--- backend/utils.py
+++ backend/utils.py
@@ -1,12 +1,12 @@
 from .status_codes import error500
 
-def render(environ, start_response,filename, context={}):
+def render(environ, start_response,filename, code='200 OK', context={}):
     try:
         response_body = ""
         with open(filename, 'r') as f:
             response_body = f.read()
             response_body = response_body.format(**context)
-        start_response('200 OK',[('Content-Type','text/html'),('Content-length',str(len(response_body)))])
+        start_response(code,[('Content-Type','text/html'),('Content-length',str(len(response_body)))])
         return [response_body.encode()]
 
     except IOError as err:
