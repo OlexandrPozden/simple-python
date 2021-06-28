630b5585cae0399dcdd64997ee7d764b9b427f20
Alex <pozden.sasha@gmail.com>
2021-06-28 15:52:32 +0300

    welcome back

--- backend/models.py
+++ backend/models.py
@@ -1,23 +1,40 @@
-import psycopg2
+from sqlalchemy import create_engine  
+from sqlalchemy import Column, String  
+from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.orm import sessionmaker
 import os
-class ConnectPg:
-    dbname="test"
-    user="postgres"
-    password="1valera1"
-    port=5432
-    conn = None
-    @classmethod
-    def connect_database(cls):
-        print("Connecting to database...")
-        try:
-            cls.conn = psycopg2.connect(dbname=cls.dbname, user=cls.user, password=cls.password, port=cls.port)
-        except:
-            print("Failed to connect to database!!!")
-        else:
-            print("Database is connected")
+password = os.getenv('DB_PASSWORD','postgres')
+db_string = "postgres-psycopg2://postgres:{}@localhost/test".format(password)
 
-HOSTNAME = os.environ.get('HOSTNAME', 'localhost')
-# class User():
-#     def __init__(self, username, password):
-#         self.username = username
-#         self.password = password
+db = create_engine(db_string)  
+base = declarative_base()
+
+class Film(base):  
+    __tablename__ = 'films'
+
+    title = Column(String, primary_key=True)
+    director = Column(String)
+    year = Column(String)
+
+Session = sessionmaker(db)  
+session = Session()
+
+base.metadata.create_all(db)
+
+# Create 
+doctor_strange = Film(title="Doctor Strange", director="Scott Derrickson", year="2016")  
+session.add(doctor_strange)  
+session.commit()
+
+# Read
+films = session.query(Film)  
+for film in films:  
+    print(film.title)
+
+# Update
+doctor_strange.title = "Some2016Film"  
+session.commit()
+
+# Delete
+session.delete(doctor_strange)  
+session.commit()  
